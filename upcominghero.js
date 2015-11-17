dfebb53f6fd038b6bb83c64df40cb86481090346
Cr4fTeXe <murcoprschl@googlemail.com>
2015-11-17 18:50:08 +0100

    Update upcominghero.js

--- upcominghero.js
+++ upcominghero.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 	console.log("UpcomingHero succesfully loaded!");
 
-	$('body').on('DOMNodeInserted', 'div.text p', function () {
-      		getChatMessage($(this));
+	$('body').on('DOMNodeInserted', 'div.text', function () {
+      getChatMessage($(this));
 	});
 
 	function getChatMessage(msg){
 		var message = msg.html();
+
+
 		console.log(message);
 	}
 })
