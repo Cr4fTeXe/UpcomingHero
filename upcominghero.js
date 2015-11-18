f0556917a46a02b7ae9a59975930d3cfe28d65b9
Cr4fTeXe <murcoprschl@googlemail.com>
2015-11-18 18:04:07 +0100

    Update upcominghero.js

--- upcominghero.js
+++ upcominghero.js
@@ -5,14 +5,6 @@ $(document).ready(function(){
 	var i = 0;
 	var currentUser = "";
 
-	$('body').on('DOMNodeInserted', 'div.text', function () {
-		var msg = getChatMessage($(this));
-		var user = getChatUser($(this));
-
-		console.log("Message: "+msg);
-		console.log("User: "+user);
-	});
-
 	function getChatMessage(msg){
 		var amsg = msg;
 		var user = amsg.children().children(".username").html();
@@ -21,7 +13,7 @@ $(document).ready(function(){
 		amsghtml = amsghtml.replace("</p>"," ");
 		amsghtml = amsghtml.replace('<a href="#" class="username">'+user+'</a>', '');
 		return amsghtml;
-	}
+		}
 	function getChatUser(user){
 		var user = user;
 		if(user.children().children(".username").html()){
@@ -30,5 +22,35 @@ $(document).ready(function(){
 		}else{
 			return currentUser;
 		}
-	}
+		}
+
+	function sayhello(u){
+		var user = u;
+		var chatmsg = "Hello "+user+"!";
+
+		var request = new XMLHttpRequest();
+		request.open('POST', 'https://api.dubtrack.fm/chat/upcoming');
+		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		request.onreadystatechange = function () {
+		  if (this.readyState === 4) {
+		    console.log('Status:', this.status);
+		    console.log('Headers:', this.getAllResponseHeaders());
+		    console.log('Body:', this.responseText);
+		  }
+		};
+		var body = "message={"+chatmsg+"}";
+		request.send(body);
+		
+		}
+
+	$('body').on('DOMNodeInserted', 'div.text', function () {
+		var msg = getChatMessage($(this));
+		var user = getChatUser($(this));
+
+		if(msg.search('!hello') >= 0){ sayhello(user);}
+
+		console.log("Message: "+msg);
+		console.log("User: "+user);
+	});
+
 })
