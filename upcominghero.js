c0d5c8cf14e43eb7aa33caadf8ff83125e7d27d2
Cr4fTeXe <murcoprschl@googlemail.com>
2015-11-20 18:31:33 +0100

    Added Message Post | Added voteskip

--- upcominghero.js
+++ upcominghero.js
@@ -1,53 +1,89 @@
 $(document).ready(function(){
-	console.log("UpcomingHero succesfully loaded!");
-	$(".left_section").remove();
+console.log("UpcomingHero succesfully loaded!");
+//$(".left_section").remove();
 
-	var i = 0;
-	var currentUser = "";
+var i = 0;
+var skip = 0;
+var skipuser = 0;
+var uservotes = 0;
+var currentUser = "";
 
-	function getChatMessage(msg){
-		var amsg = msg;
-		var user = amsg.children().children(".username").html();
-		var amsghtml = amsg.html(); //Message
-		amsghtml = amsghtml.replace("<p>","");
-		amsghtml = amsghtml.replace("</p>"," ");
-		amsghtml = amsghtml.replace('<a href="#" class="username">'+user+'</a>', '');
-		return amsghtml;
-		}
-	function getChatUser(user){
-		var user = user;
-		if(user.children().children(".username").html()){
-			currentUser = user.children().children(".username").html();
-			return user.children().children(".username").html();
-		}else{
-			return currentUser;
-		}
-		}
-function postMsg(msg){
-  var message = msg;
-  //$(".chatinout").val() = message;
-  //$(".chatbutton").trigger("click");
+function getChatMessage(msg){
+	var amsg = msg;
+	var user = amsg.children().children(".username").html();
+	var amsghtml = amsg.html(); //Message
+	amsghtml = amsghtml.replace("<p>","");
+	amsghtml = amsghtml.replace("</p>"," ");
+	amsghtml = amsghtml.replace('<a href="#" class="username">'+user+'</a>', '');
+	return amsghtml;
 }
-	function sayhello(u){
-		var user = u;
-		var chatmsg = "Hello "+user+"!";
-		postMsg(chatmsg);
-		}
-		
-	function ping(){
-		var chatmsg = "Pong!";
-		postMsg(chatmsg);
-		}
 
-	$('body').on('DOMNodeInserted', 'div.text', function () {
-		var msg = getChatMessage($(this));
-		var user = getChatUser($(this));
-if(!user == "Hero"){
-		if(msg.search('!hello') >= 0){ sayhello(user);}
-		if(msg.search('!ping') >= 0){ ping();}
-		}
-		console.log("Message: "+msg);
-		console.log("User: "+user);
+function getChatUser(user){
+	var user = user;
+	if(user.children().children(".username").html()){
+		currentUser = user.children().children(".username").html();
+		return user.children().children(".username").html();
+	}else{ return currentUser; }
+}
+
+function postMsg(msg){
+	var message = msg;
+	$("#chat-txt-message").val() = message;
+	var e = $.Event("keydown");
+	e.which = 13;
+	$("#chat-txt-message").trigger(e);
+}
+
+function sayhello(u){
+	var user = u;
+	var chatmsg = "Hello "+user+"!";
+	postMsg(chatmsg);
+}
+
+function ping(){
+	var chatmsg = "Pong!";
+	postMsg(chatmsg);
+}
+
+function skip(){
+	$(".skip-el").trigger("click");
+	postMsg("Successful Vote. Video skipped!");
+}
+
+function voteskip(action){
+	switch(action){
+		case (action == "start"): 
+			skipuser = getTotalUser();
+			votedisabled = true;
+			break;
+		case (action =="end"): 
+			if(skip > 0){skip(); uservotes = 0; skip = 0; skipuser = 0;}else{postMsg("Vote failed!"); uservotes = 0; skip = 0; skipuser = 0;}
+			break;
+		case (action == "yes"): 
+			skip++; uservotes++;
+			break;
+		case (action == "no"): 
+			skip--; uservotes++;
+			break;
+		default: 
+			console.log("No Vote-Action");
+			break;
+	}
+}
+
+$('body').on('DOMNodeInserted', 'div.text', function () {
+	var msg = getChatMessage($(this));
+	var user = getChatUser($(this));
+
+	if(!user == "Hero"){
+		if(msg.search('!hello') >= 0){ sayhello(user); }
+		if(msg.search('!ping') >= 0){ ping(); }
+		if(msg.search('!voteskip') >= 0){ voteskip("start"); }
+		if(msg.search('!skipyes') >= 0){ voteskip("yes"); }
+		if(msg.search('!skipno') >= 0){ voteskip("no"); }
+	}
+	if(skipuser == uservotes){voteskip("end"); votedisabled = false;}
+
 	});
 
 })
