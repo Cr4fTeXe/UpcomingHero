c1eab0b458c5941edf2d67d0ed506c93dcc0263a
Cr4fTeXe <murcoprschl@googlemail.com>
2015-11-25 20:13:09 +0100

    Update upcominghero.js

--- upcominghero.js
+++ upcominghero.js
@@ -8,6 +8,7 @@ $("body").append($('<div class="import-input"><div class="import-inner"><span cl
 */
 
 var i = 0, mehskipx = 0, skip = 0, skipuser = 0, uservotes = 0, currentUser = "", votedisabled = true, inAutoQueue = false, commandoption = "", autoFB = true;
+
 //Functions
 function getChatMessage(msg){
 	var amsg = msg;
@@ -38,7 +39,7 @@ function getCommandOptions(message){
 		}
 	})
 	console.log(commandoption);
-}
+	}
 function getTotalUser(){ return $(".room-user-counter").html(); }
 function postMsg(msg){
 	var message = msg;
@@ -57,7 +58,6 @@ function wb(u){
 	var chatmsg = "welcome back "+user+"!";
 	postMsg(chatmsg);
 	}
-	
 function ping(){
 	var chatmsg = "Pong!";
 	postMsg(chatmsg);
@@ -126,11 +126,17 @@ function autoJoinQueue(){ var total = getTotalUser();  if(total < 5){inAutoQueue
 function getRandomGIF(){ var gifurl = $.getJSON("https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"); return gifurl; }
 function getTagGIF(t){ var tag = t; tag = tag.replace(" ", "+");var url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="+tag; var gifurl = $.getJSON(url); return gifurl; }
 
+
+
+
 //Command-Input
-$('body').on('DOMNodeInserted', 'div.text', function () {
+$('body').on('DOMNodeInserted', 'div.text', function(){
+	
 	/*autoJoinQueue();
 	if(inAutoQueue == true && i == 0){ joinQueue(); i++;}*/
 
+	var msg = getChatMessage($(this)), user = getChatUser($(this)), now = new Date($.now());
+	
 	//Set userrank and staff
 	var rank = $(this).parent().parent().parent().attr("class"), userrank = "", staff = false, votedisabled = false;
 	if(rank.search('isOwner') >= 0){userrank = "Owner"; staff = true;}
@@ -141,9 +147,7 @@ $('body').on('DOMNodeInserted', 'div.text', function () {
 	if(rank.search('isResident-dj') >= 0){userrank = "Resident-dj";}
 	if(rank.search('isDefault') >= 0){userrank = "Default";}
 
-	var msg = getChatMessage($(this)), user = getChatUser($(this)), now = new Date($.now());
 	var commandlist = "Commands: http://upcomingrecords.com/commands/";
-	var commandlist2 = "!rave, !1738, !mehskip, !voteskip, !voteyes, !voteno, !commands / !help";
 	var staffcommandlist = "!skip, !queuePlaylist, !joinQueue, !pauseQueue, !resumeQueue, !shutdown (bot has to be restarted manually after shutdown)";
 
 	if(skipuser > 0 && uservotes > 0 && skipuser == uservotes){ votedisabled = true; voteskip("end");}
@@ -156,51 +160,49 @@ var msgoption = getCommandOptions(msg);
 		if(msg.search('!racist') >= 0 || msg.search('nigger') >= 0 || msg.search('niggur') >= 0 || msg.search('neger') >= 0 || msg.search('melon') >= 0 || msg.search('kfc') >= 0){ postMsg('https://i.ytimg.com/vi/3AzfIhs2-zo/hqdefault.jpg'); }
 		if(msg.search('back') >= 0){ wb(user); }
 
-	if(msg.search('!') >=0){
-		//if(msg.search('!calc') >= 0){ calculate(); }
-		if(msg.search('!search') >= 0 && commandoption.length > 0){ postMsg("https://en.wikipedia.org/wiki/"+commandoption); }
-		//if(msg.search('!gif') >= 0 && commandoption.length < 1){ /*postMsg(getRandomGIF());*/ console.log(getRandomGIF()); console.log("TESTgetrandomgif"); }
-		//if(msg.search('!gif') >= 0 && commandoption.length > 0){ /*postMsg(getTagGIF(commandoption));*/ console.log(getTagGIF(commandoption)); console.log("TESTgettaggif"); }
-		if(msg.search('!rules') >= 0){ postMsg("Read the rules on our website: http://upcomingrecords.com/rules/");}
-		if(msg.search('!tooLong') >= 0){ tooLong();}
-		if(msg.search('!time') >= 0){ postMsg(now);}
-		if(msg.search('!fuckyou') >= 0){ postMsg("http://i.imgur.com/dMDdQOI.gif");}
-		if(msg.search('!dealwithit') >= 0){ postMsg("http://i.imgur.com/KtIcXyL.gif");}
-		if(msg.search('!rub') >= 0){ postMsg("http://static2.fjcdn.com/thumbnails/comments/5082699+_057afdd878601db2f01dbfc4fd6b3872.gif");}
-		if(msg.search('!gachimuchi') >= 0){ postMsg("http://booru.ehkzai.com/index.php?q=/image/6665.gif");}
-		if(msg.search('!hardwareinfo') >= 0){ hardwareinfo(); }
-		if(msg.search('!hello') >= 0 && commandoption == "1"){ sayhello(user); }
-		if(msg.search('!hello') >= 0 && commandoption == "2"){ postMsg("Welcome to the UpcomingRecords Community :D"); }
-		if(msg.search('!bye') >= 0 ){ postMsg("See you later, Adios, Auf Wiedersehen, Au Revoir, Ciao, Tot ziens, Adjö, Farvel, Poka, Sayōnara, Namaste, Alweda, Näkemiin"); }
-		if(msg.search('!yolo') >= 0){ yolo(); }
-		if(msg.search('!ping') >= 0){ ping(); }
-		if(msg.search('!fb') >= 0){ fb(); }
-		if(msg.search('!dubx') >= 0){ dubx(); }
-		if(msg.search('!rave') >= 0){ postMsg("http://i.imgur.com/Rxv5Qnu.gif");}
-		if(msg.search('!1738') >= 0){ postMsg("I'm like: Hey, what's up, hello.");}
-		if(msg.search('!voteskip') >= 0 && staff == true){ votedisabledfalse(); voteskip("start"); } //Immer noch verbuggt
-		if(msg.search('!voteyes') >= 0){ voteskip("yes"); }
-		if(msg.search('!voteno') >= 0){ voteskip("no"); }
-		if(msg.search('!skip') >= 0 && staff == true){ skipvideo(); }
-		if(msg.search('!shutdown') >= 0 && staff == true && (user == "Cr4ftexe" || user == "1337")){ window.close(); }
-		if(msg.search('!queuePlaylist') >= 0 && staff == true && user == "Cr4ftexe"){ queuePlaylist(); }
-		if(msg.search('!joinQueue') >= 0 && staff == true){ joinQueue(); }
-		if(msg.search('!pauseQueue') >= 0 && staff == true){ pauseQueue(); }
-		if(msg.search('!resumeQueue') >= 0 && staff == true){ resumeQueue(); }
-		if(msg.search('!staffcommands') >= 0 && staff == true){ postMsg(staffcommandlist); }
-		if(msg.search('!commands') >= 0 || msg.search('!help') >= 0){ postMsg(commandlist); }
-	} //end of ! check
+		if(msg.search('!') >=0){
+			//if(msg.search('!calc') >= 0){ calculate(); }
+			if(msg.search('!search') >= 0 && commandoption.length > 0){ postMsg("https://en.wikipedia.org/wiki/"+commandoption); }
+			//if(msg.search('!gif') >= 0 && commandoption.length < 1){ /*postMsg(getRandomGIF());*/ console.log(getRandomGIF()); console.log("TESTgetrandomgif"); }
+			//if(msg.search('!gif') >= 0 && commandoption.length > 0){ /*postMsg(getTagGIF(commandoption));*/ console.log(getTagGIF(commandoption)); console.log("TESTgettaggif"); }
+			if(msg.search('!rules') >= 0){ postMsg("Read the rules on our website: http://upcomingrecords.com/rules/");}
+			if(msg.search('!tooLong') >= 0){ tooLong();}
+			if(msg.search('!time') >= 0){ postMsg(now);}
+			if(msg.search('!fuckyou') >= 0){ postMsg("http://i.imgur.com/dMDdQOI.gif");}
+			if(msg.search('!dealwithit') >= 0){ postMsg("http://i.imgur.com/KtIcXyL.gif");}
+			if(msg.search('!rub') >= 0){ postMsg("http://static2.fjcdn.com/thumbnails/comments/5082699+_057afdd878601db2f01dbfc4fd6b3872.gif");}
+			if(msg.search('!gachimuchi') >= 0){ postMsg("http://booru.ehkzai.com/index.php?q=/image/6665.gif");}
+			if(msg.search('!hardwareinfo') >= 0){ hardwareinfo(); }
+			if(msg.search('!hello') >= 0 && commandoption == "1"){ sayhello(user); }
+			if(msg.search('!hello') >= 0 && commandoption == "2"){ postMsg("Welcome to the UpcomingRecords Community :D"); }
+			if(msg.search('!bye') >= 0 ){ postMsg("See you later, Adios, Auf Wiedersehen, Au Revoir, Ciao, Tot ziens, Adjö, Farvel, Poka, Sayōnara, Namaste, Alweda, Näkemiin"); }
+			if(msg.search('!yolo') >= 0){ yolo(); }
+			if(msg.search('!ping') >= 0){ ping(); }
+			if(msg.search('!fb') >= 0){ fb(); }
+			if(msg.search('!dubx') >= 0){ dubx(); }
+			if(msg.search('!rave') >= 0){ postMsg("http://i.imgur.com/Rxv5Qnu.gif");}
+			if(msg.search('!1738') >= 0){ postMsg("I'm like: Hey, what's up, hello.");}
+			if(msg.search('!voteskip') >= 0 && staff == true){ votedisabledfalse(); voteskip("start"); } //Immer noch verbuggt
+			if(msg.search('!voteyes') >= 0){ voteskip("yes"); }
+			if(msg.search('!voteno') >= 0){ voteskip("no"); }
+			if(msg.search('!skip') >= 0 && staff == true){ skipvideo(); }
+			if(msg.search('!shutdown') >= 0 && staff == true && (user == "Cr4ftexe" || user == "1337")){ window.close(); }
+			if(msg.search('!queuePlaylist') >= 0 && staff == true && user == "Cr4ftexe"){ queuePlaylist(); }
+			if(msg.search('!joinQueue') >= 0 && staff == true){ joinQueue(); }
+			if(msg.search('!pauseQueue') >= 0 && staff == true){ pauseQueue(); }
+			if(msg.search('!resumeQueue') >= 0 && staff == true){ resumeQueue(); }
+			if(msg.search('!staffcommands') >= 0 && staff == true){ postMsg(staffcommandlist); }
+			if(msg.search('!commands') >= 0 || msg.search('!help') >= 0){ postMsg(commandlist); }
+		} //end of ! check
 
-    	} //end of bot user check
+    } //end of bot user check
 
 	mehskip();
-	//end of commands function
 
- 	//}
-	//);
+	});//end of commands function
 
-        setInterval(function(){ if(autoFB === true){ fb(); } }, 1000 * 60 * 70);
-
+setInterval(function(){ if(autoFB === true){ fb(); } }, 1000 * 60 * 70);
 console.log("UpcomingHero succesfully loaded! V423");
 
-})
+});
+
