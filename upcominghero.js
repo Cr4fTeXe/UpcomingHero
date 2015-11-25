37bad37b140ef99d17677bfdf7d170e671d45549
RobGoldy <nine5one@gmail.com>
2015-11-25 11:00:18 -0800

    removed test alerts
    the conclusion of the test is that the bot is not triggering when a new message is posted, only when a second or third message is posted.
    
    also the msg is a combination of all previous messages (which can get messy) but this part doesnt NEED to be fixed yet

--- upcominghero.js
+++ upcominghero.js
@@ -150,8 +150,6 @@ $('body').on('DOMNodeInserted', 'div.text', function () {
 
 	//Commands + function-calls
 var msgoption = getCommandOptions(msg);
-	alert(user);
-	alert(msg);
 	if(user != "hero" && user != "cyberpixlcraft")
 	{	
 		if(msg.search('!love') >= 0 || msg.search('love') >= 0 || msg.search(':heart:') >= 0 || msg.search('<3') >= 0){ love(); }
@@ -204,5 +202,5 @@ var msgoption = getCommandOptions(msg);
             }
             },
             1000 * 60 * 70);
-console.log("UpcomingHero succesfully loaded! VTEST2");
+console.log("UpcomingHero succesfully loaded! V422");
 })
