7f2b2d9f7f4495e47ae1058950500a76d87a5fbb
Cr4fTeXe <murcoprschl@googlemail.com>
2015-11-26 12:36:05 +0100

    Update randomgif.js

--- randomgif.js
+++ randomgif.js
@@ -1,1 +1,7 @@
+var randomgif = $.getJSON("https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC");
+var rgifarr = $.makeArray(randomgif);
+var rdata = rgifarr["data"];
+var rdataarr = $.makeArray(rdata);
+var randomurl = rdataarr["image_original_url"];
+$('body').append(randomurl);
 
