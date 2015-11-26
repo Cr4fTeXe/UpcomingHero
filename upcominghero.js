f078bfa44a634ec720a4aa7be472a38b06b077e6
RobGoldy <nine5one@gmail.com>
2015-11-25 22:36:21 -0800

    Update upcominghero.js

--- upcominghero.js
+++ upcominghero.js
@@ -211,7 +211,7 @@ setInterval(function auto() {
 //MEHSKIP
 setInterval(function(){ mehskip(); }, 4200);
 //END MEHSKIP
-alert("working2");
+alert("working5");
 console.log("UpcomingHero succesfully loaded! V42.420.1337.9001");
 
 });
